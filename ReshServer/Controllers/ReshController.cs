01222cac5d8c931b07ca39652be97496984ec2eb
Ida Kristin Hagen <idahagen@hotmail.com>
2015-09-28 02:19:19 +0200

    Project restart Before API

---
+++ ReshServer/Controllers/ReshController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using ReshServer.Services;
+using ReshServer.ReshService;
+
+namespace ReshServer.Controllers
+{
+    //usikker på om dette er riktig måte å gjøre det på 
+    [EnableCors(origins: "http://reshclient.azurewebsites.net", headers: "*", methods: "*")]
+
+    public class ReshController : ApiController
+    {
+        private ReshRepository reshRepository;
+
+        public ReshController()
+        {
+            this.reshRepository = new ReshRepository();
+        }
+
+        public ReshUnit Get(int id)
+        {
+            return this.reshRepository.getUnitById(id);
+        }
+    }
+}
---
+++ ReshServer/Services/ReshRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ReshServer.ReshService;
+
+namespace ReshServer.Services
+{
+    public class ReshRepository
+    {
+        public ReshUnit getUnitById(int id)
+        {
+            //Kan ikke tro at dette er riktig måte å gjøre det på, med passordet i klartekst og det 
+            //Usikker på om "WSHttpBinding_IReshService" er riktig 
+            ReshServiceClient client = new ReshServiceClient("WSHttpBinding_IReshService");
+            client.ClientCredentials.UserName.UserName = "webservice";
+
+            //Vet ikke hva DateTime gjør
+            ReshUnit unit = client.GetReshUnit(id, true, false, DateTime.Today);
+
+            client.Close();
+            return unit;
+        }
+    }
+}
